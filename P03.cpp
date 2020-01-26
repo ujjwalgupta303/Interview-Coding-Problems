820d06abffc24634a8e2d53bfa0dc22ad07f1c8a
ujjwalgupta303 <37122048+ujjwalgupta303@users.noreply.github.com>
2020-01-27 01:57:39 +0530

    Add files via upload

---
+++ P03.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+void check(string s);
+int main()
+{
+    string s;
+    cin>>s;
+    check(s);
+    return 0;
+}
+void check(string s)
+{
+    any_of(s.begin(),s.end(), [](char x){ return isdigit(x); })? 
+		cout << "There exists a digit" : 
+		cout << "No Digit exists"; 
+}
